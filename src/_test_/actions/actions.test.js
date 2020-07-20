27f35aba902e226ec2d839ff6b21f3367f0026ce
Maria Fernanda Mendoza Vargas - Ceiba Software <maria.mendoza@ceiba.com.co>
2020-07-19 19:46:50 -0500

    Probando Actions

---
+++ src/_test_/actions/actions.test.js
@@ -0,0 +1,24 @@
+import actions from '../../actions';
+import ProductMock from '../../_mocks_/ProductMock';
+
+describe('Actions',()=>{
+    const payload = ProductMock;
+    test('addToCart Action  ', () => {  
+        const expected = {
+            type : 'ADD_TO_CART',
+            payload,
+        };
+
+        expect(actions.addToCart(payload)).toEqual(expected);
+    });
+
+    test('removeFromCart Action  ', () => {  
+        const expected = {
+            type : 'REMOVE_FROM_CART',
+            payload,
+        };
+
+        expect(actions.removeFromCart(payload)).toEqual(expected);
+    })
+    
+})
